30a6516b92d7faad29f2659aa14b6fc814110dd3
achimwestermann <achimwestermann@users.sourceforge.net>
2004-10-25 08:26:04 +0000

    Choosable guessing of charsets: If jchardet fails to narrow down the set of possible charsets, it may (if chosen) return a potential one.

--- src/cpdetector/io/JChardetFacade.java
+++ src/cpdetector/io/JChardetFacade.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.nio.charset.UnsupportedCharsetException;
 
 import org.mozilla.intl.chardet.nsDetector;
 import org.mozilla.intl.chardet.nsICharsetDetectionObserver;
@@ -42,18 +43,24 @@ import org.mozilla.intl.chardet.nsPSMDetector;
 public final class JChardetFacade extends AbstractCodepageDetector implements nsICharsetDetectionObserver {
   private static JChardetFacade instance = null;
 
-  private static nsDetector det = new nsDetector(nsPSMDetector.ALL);
+  private static nsDetector det;
 
   private static byte[] buf = new byte[4096];
 
   private Charset codpage = null;
+  
+  private boolean guessing = true;
+  
+  private int amountOfVerifiers =0;
 
   /**
    *  
    */
   private JChardetFacade() {
     super();
+    det = new nsDetector(nsPSMDetector.ALL);
     det.Init(this);
+    this.amountOfVerifiers = det.getProbableCharsets().length;
   }
 
   public static JChardetFacade getInstance() {
@@ -97,14 +104,53 @@ public final class JChardetFacade extends AbstractCodepageDetector implements ns
     } while (len != -1 && !done);
     det.DataEnd();
     if (this.codpage == null) {
-      // The callback Notify will not have been called...
-      // looked in method isAscii... and found,
-      // that the leading bits above the 7 one are filtered.
-      // So it is US-ASCII detection.
-      ret = UnknownCharset.getInstance();
+      if(this.guessing){
+        ret = guess();
+      }
+      else{
+        ret = UnknownCharset.getInstance();
+      }
     }
     else {
       ret = this.codpage;
+    }
+    return ret;
+
+  }
+
+  /**
+   * 
+   */
+  private Charset guess() {
+    Charset ret = null;
+    //TODO: remove output.
+    String[] possibilities = det.getProbableCharsets(); 
+    System.out.println("Possible Charsets: ");
+    for(int i=0;i<possibilities.length;i++){
+      System.out.println(possibilities[i]);
+    }
+    /*
+     * Detect US-ASCII by the fact, that no exclusion 
+     * of any Charset was possible.
+     */
+    if(possibilities.length == this.amountOfVerifiers){
+      ret = Charset.forName("US-ASCII");
+    }
+    else{
+      // He should better return an Array of length zero!
+      String check = possibilities[0];
+      if(check.equalsIgnoreCase("nomatch")){
+        ret = UnknownCharset.getInstance();
+      }
+      else{
+        for(int i=0;ret == null && i<possibilities.length;i++){
+          try{
+          ret = Charset.forName(possibilities[i]);         
+          }catch(UnsupportedCharsetException uce){
+            ret = UnsupportedCharset.forName(possibilities[i]);
+          }
+        }
+      }
     }
     return ret;
 
@@ -122,5 +168,43 @@ public final class JChardetFacade extends AbstractCodepageDetector implements ns
   public void Reset() {
     det.Reset();
     this.codpage = null;
+  }
+  /**
+   * @return Returns the guessing.
+   */
+  public boolean isGuessing() {
+    return guessing;
+  }
+  /**
+   * <p>
+   * If it was impossible to narrow down possible results to one, 
+   * an internal set of possible character encodings exists. 
+   * By setting guessing to true, the call to {@link #detectCodepage(InputStream, int)} 
+   * and {@link #detectCodepage(URL)} will return an arbitrary 
+   * possible Charset.  
+   * </p>
+   * <p>
+   *  Currently the following precedence is implemented to choose the 
+   *  possible Charset: 
+   *  <ol>
+   *   <li>
+   *   If US-ASCII is possible, it is chosen. 
+   *   <li>
+   *   If US-ASCII is not possible, the first supported  one in the set of possible 
+   *   charsets is returned. No information about the semantics of the order 
+   *   in that list is available. If no possibility is supported, 
+   *   an instance of {@link UnsupportedCharset} is returned.
+   *  </ol>
+   *   ASCII indeed is never detected as possible: No internal 
+   *   verifier exists for ASCII, as all Charsets support ASCII. The 
+   *   possibility of ASCII is detected, when no Charset has been excluded: 
+   *   The amount of possible Charsets is equal to the amount of all 
+   *   detectable Charsets.  
+   * </p>
+   * 
+   * @param guessing The guessing to set.
+   */
+  public synchronized void setGuessing(boolean guessing) {
+    this.guessing = guessing;
   }
 }
