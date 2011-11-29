d563ca382704bc3899018b68842483622db5edfc
achimwestermann <achimwestermann@users.sourceforge.net>
2011-11-29 20:49:18 +0000

    - removed unused imports.

--- test/info/monitorenter/cpdetector/TestCodepageProcessor.java
+++ test/info/monitorenter/cpdetector/TestCodepageProcessor.java
@@ -49,11 +49,6 @@
 package info.monitorenter.cpdetector;
 
 import info.monitorenter.util.ExceptionUtil;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
 import junit.framework.Assert;
 
 import org.junit.Test;
